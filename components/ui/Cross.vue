b09cfed330b7b88dcd22dd8dcc526d80e6053ccb
Vladislav Husakov <vlad@unicornpower.space>
2022-07-28 15:34:22 +0300

    upd

---
+++ components/ui/Cross.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+.cross(
+	:class="'cross_' + color"
+)
+	include ../../assets/svg/cross.svg
+</template>
+
+<script>
+
+export default {
+	props: {
+		color: {
+			type: String,
+			default: 'balck'
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.cross {
+
+	&_black {
+		svg path {
+			fill: black;
+		}
+	}
+
+	&_green {
+		svg path {
+			fill: #90EE90;
+		}
+	}
+}
+</style>
