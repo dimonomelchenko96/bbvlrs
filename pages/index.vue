6f4398ffe8a2c3be09679b99efa111b8f79a1755
Vladislav Husakov <vlad@unicornpower.space>
2022-08-02 11:43:57 +0300

    api doc

--- pages/index.vue
+++ pages/index.vue
@@ -13,12 +13,20 @@ import Enter from "~/components/ui/Enter";
 export default {
 	name: 'IndexPage',
 	async asyncData({ $api }) {
-		// const book = await $api.bible.book();
-		const chaptersResp = await $api.bible.chapters();
-
+		// Отримання книжок
+		const booksResp = await $api.bible.books();
+		const bookIdExample = booksResp.data.data[1].id;
+		// Отримання книг з главами
+		const booksWithChaptersResp = await $api.bible.booksWithChapters();
+		// Отримання глав однієї книги
+		const chaptersResp = await $api.bible.chapters(bookIdExample);
 		const chapterIdExample = chaptersResp.data.data[0].id
+		// Отримання глави з віршами у вигляді HTML
 		const chapterResp = await $api.bible.chapter(chapterIdExample);
 		const chapterHTML = chapterResp.data.data.content;
+		// Прилад виккористання пошуку
+		const exampleQuery = 'adam';
+		const exampleSearch = await $api.bible.search(exampleQuery);
 
 	},
 	data() {
