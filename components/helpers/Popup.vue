fd1c30ab0eea6b857ff79b0ca5cfd9a5bc755810
dimonomelchenko96 <dimonomelchenko1@gmail.com>
2022-08-04 18:20:49 +0300

    add character search

--- components/helpers/Popup.vue
+++ components/helpers/Popup.vue
@@ -33,9 +33,10 @@ export default {
 	position: relative;
 	padding-top: m(56);
 	background: url(~/assets/img/Background.jpg) center center/cover no-repeat;
-
+	height: calc(var(--vh) * 100);
 	&__header {
 		position: fixed;
+		z-index: 102;
 		top: 0;
 		left: 0;
 		width: 100%;
@@ -47,6 +48,7 @@ export default {
 
 	&__content {
 		height: 100%;
+		overflow: scroll;
 	}
 
 	// &__pagination {
