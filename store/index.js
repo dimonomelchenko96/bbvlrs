e766bf3148a5734f4ac84160cdda81022ed1ed30
Vladislav Husakov <vlad@unicornpower.space>
2022-08-10 16:58:03 +0300

    u

--- store/index.js
+++ store/index.js
@@ -1,19 +1,11 @@
 export const state = () => ({
 	allMembers: false,
-	mainShow: true,
-<<<<<<< HEAD
 	roadmap: false,
 	collaborations: false,
 	aboutProject: false,
 });
-=======
-})
->>>>>>> develop_major
 
 export const mutations = {
-	hideMain(state) {
-		state.mainShow = false;
-	},
 	roadMapToggle(state) {
 		state.roadmap = !state.roadmap;
 	},
@@ -23,7 +15,6 @@ export const mutations = {
 	aboutProjectToggle(state) {
 		state.aboutProject = !state.aboutProject;
 	},
-
 	allMembersToggle(state) {
 		state.allMembers = !state.allMembers;
 	},
